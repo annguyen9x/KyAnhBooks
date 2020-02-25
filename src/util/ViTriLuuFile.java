86ffe22071c5bac49c7e676b148c07cbb4fe7be5
An Nguyen <annguyen.htvn@gmail.com>
2020-02-25 17:17:09 +0700

    update ViTriLuuFile

---
+++ src/util/ViTriLuuFile.java
@@ -0,0 +1,5 @@
+package util;
+
+public class ViTriLuuFile {
+	public static final String VITRILUU_HINHANH = "D:/HK119/JAVA/DACN/DACN/deployHosting/SachKyAnh/WebContent/view/user/static/img/sanpham";
+}
